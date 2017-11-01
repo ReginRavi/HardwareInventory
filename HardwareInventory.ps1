c15e0e3afb80f9fc6a85d6cdf827d93bf2656bc9
Ravi <Regin.Ravi@unisys.com>
2017-11-01 10:37:53 +0530

    enter

---
+++ HardwareInventory.ps1
@@ -0,0 +1,2 @@
+$HardwareInventoryID = '{00000000-0000-0000-0000-000000000001}'
+Get-CimInstance -Namespace 'Root\CCM\INVAGT' -ClassName 'InventoryActionStatus' -Filter "InventoryActionID='$HardwareInventoryID'" | Remove-CimInstance
