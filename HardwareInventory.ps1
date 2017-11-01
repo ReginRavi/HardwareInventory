4d4367e03eed9e7b244617c4b72e88630770f52d
Ravi <Regin.Ravi@unisys.com>
2017-11-01 10:45:58 +0530

    Added

--- HardwareInventory.ps1
+++ HardwareInventory.ps1
@@ -1,2 +1,3 @@
+$comp="localhost"
 $HardwareInventoryID = '{00000000-0000-0000-0000-000000000001}'
-Get-CimInstance -Namespace 'Root\CCM\INVAGT' -ClassName 'InventoryActionStatus' -Filter "InventoryActionID='$HardwareInventoryID'" | Remove-CimInstance
+Get-CimInstance -ComputerName $comp -Namespace 'Root\CCM\INVAGT' -ClassName 'InventoryActionStatus' -Filter "InventoryActionID='$HardwareInventoryID'" | Remove-CimInstance
